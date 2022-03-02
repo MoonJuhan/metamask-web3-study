187eaa9e3a27a57d616fdb604bd67c0f6762c26f
Moon Juhan <codemike@naver.com>
2022-03-02 10:59:15 +0900

    feat: add send transaction page

---
+++ src/components/views/ViewTransaction.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="view-transaction">
+    <h2>Now Network : {{ networkName }}</h2>
+    <h2>address : {{ address }}</h2>
+
+    <input type="text" v-model="contractAddress" placeholder="contract address" />
+    <input type="text" v-model="gas" placeholder="gas" />
+    <input type="text" v-model="transactionValue" placeholder="transactionValue" />
+    <input type="file" @change="handleFileChange" />
+
+    <button @click="onClickGetAddress">Get Address</button>
+    <button @click="onClickSendTransaction" :disabled="!address">Send Transaction</button>
+  </div>
+</template>
+
+<script>
+import Web3 from 'web3'
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const web3 = new Web3(window.ethereum)
+
+    const networkName = ref(null)
+    const address = ref(null)
+    const contractAddress = ref(null)
+    const contractABI = ref(null)
+    const gas = ref('300000')
+    const transactionValue = ref('20000000000000')
+
+    const setNetworkName = async () => {
+      const chainId = await ethereum.request({ method: 'eth_chainId' })
+
+      const chainNames = [
+        {
+          chainId: '0x1',
+          chainName: 'eth',
+        },
+        {
+          chainId: '0x4',
+          chainName: 'eth_rinkeby',
+        },
+      ]
+
+      networkName.value = 'unknown Network'
+
+      if (chainNames.find((el) => el.chainId === chainId))
+        networkName.value = chainNames.find((el) => el.chainId === chainId).chainName
+    }
+
+    const handleFileChange = (e) => {
+      const reader = new FileReader()
+
+      reader.onload = (e) => {
+        contractABI.value = JSON.parse(e.target.result)
+      }
+      reader.readAsText(e.target.files[0])
+    }
+
+    const onClickGetAddress = async () => {
+      address.value = null
+
+      try {
+        address.value = (await web3.eth.requestAccounts())[0]
+      } catch (error) {
+        alert('ERROR')
+      }
+    }
+
+    onMounted(() => {
+      setNetworkName()
+    })
+
+    const onClickSendTransaction = async () => {
+      const web3 = new Web3(window.ethereum)
+
+      const PabSixth = new web3.eth.Contract(contractABI.value, contractAddress.value)
+
+      const transactionParameters = {
+        to: contractAddress.value,
+        gas: gas.value,
+        from: address.value,
+        data: PabSixth.methods.mintPabForPreSale(1).encodeABI(),
+        value: transactionValue.value,
+      }
+
+      ethereum.request({ method: 'eth_sendTransaction', params: [transactionParameters] })
+    }
+
+    return {
+      networkName,
+      address,
+      contractAddress,
+      gas,
+      transactionValue,
+      handleFileChange,
+      onClickGetAddress,
+      onClickSendTransaction,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.view-transaction {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  h2 {
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+
+  input,
+  button {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+}
+</style>
--- src/router/routes-async.js
+++ src/router/routes-async.js
@@ -7,6 +7,10 @@ const routesBasic = [
     path: '/login',
     component: () => import(/* webpackChunkName: 'view-login' */ '../components/views/ViewLogin.vue'),
   },
+  {
+    path: '/transaction',
+    component: () => import(/* webpackChunkName: 'view-transaction' */ '../components/views/ViewTransaction.vue'),
+  },
 ]
 
 const routesAsync = [...routesBasic].map((o) => o)
